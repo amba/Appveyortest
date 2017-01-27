49cfb9dbc4ead2f8203cfb876c9b7da0affb2305
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2017-01-27 22:00:45 +0100

    alien::gnuplot

---
+++ gnuplot_pipe.pl
@@ -0,0 +1,19 @@
+use 5.020;
+use warnings;
+use strict;
+
+use Alien::Gnuplot;
+
+my $gnuplot = $Alien::Gnuplot::executable;
+
+say "gnuplot: $gnuplot";
+
+my $version = $Alien::Gnuplot::version;
+
+say "gnuplot version: $version";
+
+my %terms = %Alien::Gnuplot::terms;
+
+for my $term (keys %terms) {
+    say "$term: $terms{$term}";
+}
